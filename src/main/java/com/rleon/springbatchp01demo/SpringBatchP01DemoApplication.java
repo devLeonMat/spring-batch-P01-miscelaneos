dbd732c0cc2dcde164b49ad5ae2b577dc9fca755
leon matias roslin <leonmatias1991@gmail.com>
2021-02-17 09:37:58 -0500

    subiendo primera version

---
+++ src/main/java/com/rleon/springbatchp01demo/SpringBatchP01DemoApplication.java
@@ -0,0 +1,13 @@
+package com.rleon.springbatchp01demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchP01DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchP01DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rleon/springbatchp01demo/SpringBatchP01DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.springbatchp01demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchP01DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
